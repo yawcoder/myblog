67a1bbcfc29c358ce83ee107a8d0cc9a2ec3e826
yawcoder <isaac16anim@gmail.com>
2024-05-04 15:58:59 +0000

    dotenv configured with firebase APIs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import wavy from "../assets/wavy.jpg"
+import React, { useEffect, useState } from 'react'
+import wavy from "../assets/wavy.png"
+import { db } from '../firebase-config'
+import { collection, getDocs } from 'firebase/firestore'
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const postCollectionRef = collection(db, "posts")
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postCollectionRef);
+      setPosts(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+    }
+    getPosts();
+  }, [])
+
+
+
+
+
   return (
-    <div>
+    <div className="w-full">
       <img src={wavy} className="w-screen"/>
+      <div className="w-[90%] mx-auto ">
+        <h2 className="uppercase font-bold">Recently Published Articles</h2>
+        {posts.map((post) => {
+          console.log(post.content)
+        })}
+      </div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { FaGithub} from 'react-icons/fa';
-import logo from "../assets/logo.jpg"
+import logo from "../assets/logo.png"
 import { LiaBarsSolid } from "react-icons/lia";
 import { GoRss } from "react-icons/go";
 
@@ -11,18 +11,18 @@ const Navbar = () => {
 
 
   return (
-    <nav className="py-4 flex justify-between bg-orange-100 md:px-20 md:py-14">
-        <div>
+    <nav className="py-6 flex justify-between bg-orange-100 md:px-10 md:py-10">
+        <div className="w-1/3 md:w-1/5"> 
           <Link to="/">
-            <img src={logo} alt="logo" className="ml-5" width={100}/>
+            <img src={logo} alt="logo" className="ml-5 w-full"/>
           </Link>
         </div>
-        <ul onClick={() => {setShowLinks()}} className={`${showLinks ? "absolute top-0 right-0 w-full h-screen text-center justify-center flex flex-col space-y-16 backdrop-blur-md duration-200 md:static md:flex-row md:w-[50%] md:h-5 md:justify-start md:space-y-0 md:gap-10" : "absolute top-0 right-0 w-0 h-screen text-center justify-center flex flex-col space-y-16 backdrop-blur-md duration-200 md:static md:flex-row md:w-[50%] md:h-5 md:justify-start md:space-y-0 md:gap-10"}`}>
+        <ul onClick={() => {setShowLinks()}} className={`${showLinks ? "absolute top-0 right-0 w-full h-screen text-center justify-center flex flex-col space-y-16 backdrop-blur-md duration-200 md:static md:flex-row md:w-[50%] md:h-10 md:space-y-0 md:gap-10" : "absolute top-0 right-0 w-0 h-screen text-center justify-center flex flex-col space-y-16 backdrop-blur-md duration-200 overflow-x-hidden md:static md:flex-row md:w-3/5 md:h-10 md:space-y-0 md:gap-10"}`}>
           <li className="text-4xl md:text-lg"><Link to="/tags">Tags</Link></li>
           <li className="text-4xl md:text-lg"><Link to="/about">About</Link></li>
           <li className="text-4xl md:text-lg"><Link to="/contact">Contact</Link></li>
         </ul>
-        <div className="flex">
+        <div className="flex md:w-1/5 md:justify-end">
           <button className="text-2xl"><FaGithub/></button>
           <button className="text-2xl hidden md:inline-block ml-5"><GoRss/></button>
           <button onClick={() => {setShowLinks(true)}} className="text-4xl ml-4 mr-5 md:hidden"><LiaBarsSolid/></button>
