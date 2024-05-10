55e1dd126aded7e0c7f3fc1b3e60eda2c22f133e
yawcoder <isaac16anim@gmail.com>
2024-05-10 15:20:32 +0000

    post page initialised. Next I need to tags under the article heading

--- src/components/PostPage.jsx
+++ src/components/PostPage.jsx
@@ -1,9 +1,52 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { useParams } from 'react-router-dom';
+import { doc, query, where, collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase-config';
+import parser from "html-react-parser";
 
 const PostPage = () => {
+  const pathNameObject = useParams();
+  const pathName = `${pathNameObject.pathName}`;
+  const [post, setPost] = useState(null)
+
+  useEffect(() => {
+    const getPost = async () => {
+      const postCollectionRef = collection(db, "Posts");
+      const q = query(postCollectionRef, where("pathName", "==", pathName));
+      const querySnapShot = await getDocs(q);
+      querySnapShot.forEach((doc) => {
+        setPost(doc.data());
+      })
+    }
+    getPost();
+  }, [])
+  
+
+
+
   return (
-    <div>PostPage</div>
+    <HelmetProvider>
+        <div className="w-full">
+            <Helmet>
+                <title>
+                    
+                </title>
+            </Helmet>
+            {post && (
+              <div className="w-full">
+                <section className="bg-orange-200 pt-16 pb-10 px-5 w-full">
+                  <h1 className="font-bold mt-0">{post.title}</h1>
+                </section>
+                <section className="py-10 px-5">
+                  <article className="text-lg">
+                    {parser(post.content)}
+                  </article>
+                </section>
+              </div>
+            )}
+        </div>
+    </HelmetProvider>
   )
 }
 
